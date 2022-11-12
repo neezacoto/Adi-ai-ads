75722921a66ad1814fb97b5c21f29d21d966750f
Sai Nayunipati <53490935+sai-nayunipati@users.noreply.github.com>
2022-11-12 13:21:00 -0500

    Add user registration

--- back-end/adi/backend/urls.py
+++ back-end/adi/backend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('generate/', views.generate, name='generate'),
     path('buy/', views.buy, name='buy'),
-    path('output/', views.output, name='output'),
-    path('num-credits/', views.num_credits, name='num-credits',)
+    path('output/<str:resource_path>', views.output, name='output'),
+    path('num-credits/', views.num_credits, name='num-credits',),
+    path('register/<str:username>/<str:password>', views.register, name='register')
 ]
--- back-end/adi/backend/views.py
+++ back-end/adi/backend/views.py
@@ -1,16 +1,36 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import base64
 from django.contrib.auth import authenticate
 from .models import Profile
+from django.contrib.auth.models import User
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
+@csrf_exempt
 def generate(request):
-    return HttpResponse("Generate")
+    recieved_json_data = json.loads(request.body.decode("utf-8"))
+    
+    auth_header = request.META['HTTP_AUTHORIZATION']
+    encoded_credentials = auth_header.split(' ')[1]  # Removes "Basic " to isolate credentials
+    decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8").split(':')
+    username = decoded_credentials[0]
+    password = decoded_credentials[1]
+    feed_bot = authenticate(username=username, password=password)
+    # if the credentials are correct, then the feed_bot is not None, but is a User object.
+    
+    p = Profile.objects.get(user=feed_bot)
+    
+    if p.credits_remaining >= 10:
+        p.credits_remaining -= 10
+        p.save()
+        
+        return JsonResponse({"wasSuccessful": True, "resource_link": "example.org/1"})
+    
+    return JsonResponse({"wasSuccessful": False})
 
 
 # The user gets a certain number of credits
@@ -33,8 +53,8 @@ def buy(request):
     return HttpResponse(p.credits_remaining)
 
 
-def output(request):
-    return HttpResponse("Output")
+def output(request, resource_path):
+    return FileResponse(open(f'processed/{resource_path}', 'rb'))
 
 
 def num_credits(request):
@@ -47,3 +67,11 @@ def num_credits(request):
     
     p = Profile.objects.get(user=feed_bot)
     return HttpResponse(p.credits_remaining)
+
+def register(request, username, password):
+    user = User.objects.create_user(username=username, password=password)
+    user.save()
+    
+    profile = Profile(user=user, credits_remaining=3)
+    profile.save()
+    return HttpResponse("Congratulations! You have a new account with " + str(profile.credits_remaining) + " credits!")
