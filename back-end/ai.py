ddb9f5c394c7b171f54fb8b237cb52c1ea05686f
Benjamin Lee <55467050+mslee300@users.noreply.github.com>
2022-11-12 14:20:35 -0500

    Update ai.py

--- back-end/ai.py
+++ back-end/ai.py
@@ -3,7 +3,7 @@ import requests
 
 def generate_slogan(product_desc, target_audience):
   # Takes product description and target audience as string, returns string slogan
-  openai.api_key = "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"
+  openai.api_key = "" #ADD API KEY HERE
   response = openai.Completion.create(model="text-davinci-002", prompt=f"Write a one-line creative ad for the following product to run on Instagram aimed at {target_audience}:\n\nProduct: {product_desc}.", temperature=0.5, max_tokens=60)
   return response['choices'][0]['text'].strip()
   
@@ -12,7 +12,7 @@ def generate_image(style, image_description):
   """Takes in style and image description as strings, returns string file path of image
      style must be one of following: 'random', 'photo', '3d-render', 'cartoon', 'painting', 'hand-drawn'.
   """
-  openai.api_key = "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"
+  openai.api_key = "" #ADD API KEY HERE
   if style == "random":
     image_style = ""
   elif style == "photo":
