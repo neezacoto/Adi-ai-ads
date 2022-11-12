4324ddb3426a08026772f7174a9cd68353806314
Sai Nayunipati <53490935+sai-nayunipati@users.noreply.github.com>
2022-11-12 12:35:17 -0500

    Add Profile + Advertisement model, credit buying

--- back-end/adi/backend/models.py
+++ back-end/adi/backend/models.py
@@ -5,3 +5,16 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     credits_remaining = models.IntegerField()
+
+class Avertisement(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    original_image_path = models.CharField(max_length=200)
+    final_image_path = models.CharField(max_length=200)
+    
+    # style = models.CharField(max_length=200)
+    # image_description = models.CharField(max_length=500)
+    
+    # product_description = models.CharField(max_length=500)
+    # target_audience = models.CharField(max_length=500)
+    
+    # slogan_text = models.CharField(max_length=500)
--- back-end/adi/backend/views.py
+++ back-end/adi/backend/views.py
@@ -1,5 +1,9 @@
+import json
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
+import base64
+from django.contrib.auth import authenticate
+from .models import Profile
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -9,8 +13,24 @@ def generate(request):
     return HttpResponse("Generate")
 
 
+# The user gets a certain number of credits
+@csrf_exempt
 def buy(request):
-    return HttpResponse("Buy")
+    recieved_json_data = json.loads(request.body.decode("utf-8"))
+    
+    auth_header = request.META['HTTP_AUTHORIZATION']
+    encoded_credentials = auth_header.split(' ')[1]  # Removes "Basic " to isolate credentials
+    decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8").split(':')
+    username = decoded_credentials[0]
+    password = decoded_credentials[1]
+    feed_bot = authenticate(username=username, password=password)
+    # if the credentials are correct, then the feed_bot is not None, but is a User object.
+    
+    p = Profile.objects.get(user=feed_bot)
+    p.credits_remaining += int(recieved_json_data['num_credits'])
+    p.save()
+ 
+    return HttpResponse(p.credits_remaining)
 
 
 def output(request):
@@ -18,4 +38,12 @@ def output(request):
 
 
 def num_credits(request):
-    return HttpResponse("Num credits")
+    auth_header = request.META['HTTP_AUTHORIZATION']
+    encoded_credentials = auth_header.split(' ')[1]  # Removes "Basic " to isolate credentials
+    decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8").split(':')
+    username = decoded_credentials[0]
+    password = decoded_credentials[1]
+    feed_bot = authenticate(username=username, password=password)
+    
+    p = Profile.objects.get(user=feed_bot)
+    return HttpResponse(p.credits_remaining)
